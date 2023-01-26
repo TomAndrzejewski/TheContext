d31b73a5c521af4662c3675c9059a14d7b4ad8ae
pwoch <43825825+pwoch@users.noreply.github.com>
2023-01-26 17:40:32 +0100

    Fighting

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
 
+public enum CharacterType { Wizard, Worker };
+
 public class PlayerController : MonoBehaviour
 {
+    public CharacterType characterType;
     public float speed;
     public bool canMove;
     private Rigidbody2D rb;
     private Vector2 direction;
+    private  ParticleSystem particle;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-    }
+        particle = GetComponent<ParticleSystem>();
+  }
 
     // Update is called once per frame
     void Update()
@@ -20,11 +25,33 @@ public class PlayerController : MonoBehaviour
         var directionX = Input.GetAxisRaw("Horizontal");
         var directionY = Input.GetAxisRaw("Vertical");
         direction = new Vector2(directionX, directionY);
-    }
+        if (characterType == CharacterType.Worker && !particle.isPlaying) Rotate();
+        if (Input.GetKeyDown(KeyCode.Space)) Attack();
+  }
 
 	void FixedUpdate()
 	{
         if (!canMove) return;
-        rb.velocity = new Vector2(direction.x * speed, direction.y * speed);
+        rb.velocity = new Vector2(direction.x * speed, direction.y * speed); 
+	}
+
+  void Rotate()
+  {
+    Vector3 mousePosition = Input.mousePosition;
+    mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+    Vector3 direction = new Vector3(
+        mousePosition.x - transform.position.x,
+        mousePosition.y - transform.position.y,
+        0.0f
+    );
+
+    transform.right = direction;
+  }
+
+  void Attack()
+	{
+    if(!particle.isPlaying)
+      particle.Play();
 	}
 }
